998fc4f44590c9024535bab0946019f98a93514d
trumptheduck <nhatyt123@gmail.com>
2021-06-18 11:52:25 +0700

    Debug update

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -4,16 +4,17 @@ const accessTokenKey = "this_is_key_for_development";
 
 module.exports = async (req, res, next) => {
     const jwtToken = req.query.token;
+    console.log("token",jwtToken);
+    console.log(jwt)
     if (!jwt||jwtToken===null||jwtToken===undefined) {
       return res.send("Access Denied!")
     }
     const decodedToken = jwt.decode(jwtToken, accessTokenKey);
+    console.log(decodedToken);
     if (decodedToken === null) {
         return res.send("Access Denied!")
     }
-    console.log(decodedToken);
-    console.log("token",jwtToken);
-    console.log(jwt)
+
     User.findOne({ _id: decodedToken?.userId }).exec((err, user) => {
       if (err) {
         console.log(err)
