446553adc9e4065c7b85f929b687e6f99688c135
trumptheduck <nhatyt123@gmail.com>
2021-06-18 11:46:57 +0700

    Temp disable further credential check

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -2,7 +2,27 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const accessTokenKey = "this_is_key_for_development";
 
-module.exports = async (req, res, next) => {
+// module.exports = async (req, res, next) => {
+//     const jwtToken = req.query.token;
+  
+//     if (!jwt||jwtToken===null||jwtToken===undefined) {
+//       return res.send("Access Denied!")
+//     }
+//     const decodedToken = jwt.decode(jwtToken, accessTokenKey);
+//     if (decodedToken === null) {
+//         return res.send("Access Denied!")
+//     }
+  
+//     User.findOne({ _id: decodedToken?.userId }).exec((err, user) => {
+//       if (err) {
+//         return res.send("Access Denied!")
+//       }
+//       if (user) {
+//         next()
+//       }
+//     });
+//   };
+  module.exports = async (req, res, next) => {
     const jwtToken = req.query.token;
   
     if (!jwt||jwtToken===null||jwtToken===undefined) {
@@ -12,13 +32,5 @@ module.exports = async (req, res, next) => {
     if (decodedToken === null) {
         return res.send("Access Denied!")
     }
-  
-    User.findOne({ _id: decodedToken?.userId }).exec((err, user) => {
-      if (err) {
-        return res.send("Access Denied!")
-      }
-      if (user) {
         next()
-      }
-    });
   };
